d9f4023f20d99358b4a0cace346cc99f9df1c8a0
Rick Dumpleton <dev.bites.uk@gmail.com>
2021-03-28 17:29:20 +0900

    add: transaction list component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,6 +11,7 @@ import {
 
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
   const classes = useStyles();
@@ -36,7 +37,7 @@ const Main = () => {
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List /> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
