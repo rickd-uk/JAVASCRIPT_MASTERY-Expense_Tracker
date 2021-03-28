18581ae79bb782b2fbb2f1e9216372d06eb3ebe4
Rick <rick@MacBook-Pro.local>
2021-03-28 15:44:52 +0900

    add: setup basic main, income & expense cards

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from '@material-ui/core';
+
+import useStyles from './styles';
+import Form from './Form/Form';
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title='Expense Tracker' subHeader='Powered by Speechly' />
+
+      <CardContent>
+        <Typography align='center' variant='h5'>
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          style={{ lineHeight: '1.5em', mairginTop: '20px' }}>
+          {/* InfoCard ... */}
+          Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* <List /> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
